762d663d85bb28ef42eac93b9ee0caa042edfd90
amit fridman <amitf41@gmail.com>
2020-12-27 11:32:40 +0200

    adding the projects files

---
+++ Avocado/Avocado/urls.py
@@ -0,0 +1,40 @@
+"""Avocado URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import path, include
+
+from django.conf.urls import url
+from django.views.generic import TemplateView
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+
+class SimpleStaticView(TemplateView):
+    def get_template_names(self):
+        return [self.kwargs.get('template_name') + ".html"]
+
+    def get(self, request, *args, **kwargs):
+        return super(SimpleStaticView, self).get(request, *args, **kwargs)
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('avocados.urls')),
+    url(r'^(?P<template_name>\w+)$', SimpleStaticView.as_view(template_name='anjular_site'), name='avocados'),
+]
---
+++ Avocado/avocados/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+import datetime
+import pandas as pd
+
+regions = pd.read_csv('avocados/avocado.csv')['region'].values
+
+
+class Avocado(models.Model):
+    total_volume = models.FloatField()
+    total_bags = models.FloatField()
+    small_bags = models.FloatField()
+    large_bags = models.FloatField()
+    x_large_bags = models.FloatField()
+    type = models.CharField(max_length=15, choices=[('conventional', 0), ('organic', 1)])
+    year = models.IntegerField(choices=[(i, i) for i in range(1950, datetime.datetime.now().year + 1)])
+    region = models.CharField(max_length=20, choices=[(regions[i],i) for i in range(len(regions))])
+    t_4046 = models.FloatField()
+    t_4225 = models.FloatField()
+    t_4770 = models.FloatField()
+    average_price = models.FloatField(default=0)
+
+
+
+
