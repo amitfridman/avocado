b14942baf8ac8d897c123eb4346ac8994592716a
amitfridman <amitf41@gmail.com>
2020-12-31 09:43:23 +0200

    Update settings.py

--- Avocado/Avocado/settings.py
+++ Avocado/Avocado/settings.py
@@ -126,6 +126,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT= os.path.join(BASE_DIR,'avocados/static')
+STATIC_ROOT= os.path.join(PROJECT_DIR,'avocados/static')
 
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR / "avocados/static"]
