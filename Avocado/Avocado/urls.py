8d490bdf68b42905f2b696535f3d02e97c836995
amitfridman <amitf41@gmail.com>
2020-12-31 08:39:50 +0200

    Update urls.py

--- Avocado/Avocado/urls.py
+++ Avocado/Avocado/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from . import settings
 from django.contrib import admin
 from django.urls import path, include
 
@@ -37,4 +37,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('avocados.urls')),
     url(r'^(?P<template_name>\w+)$', SimpleStaticView.as_view(template_name='anjular_site'), name='avocados'),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
