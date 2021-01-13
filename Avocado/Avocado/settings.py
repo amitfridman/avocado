5cbb40782d0a8658e80160a5b62a81e1103905b4
amitfridman <amitf41@gmail.com>
2021-01-13 09:37:16 +0200

    Update settings.py

--- Avocado/Avocado/settings.py
+++ Avocado/Avocado/settings.py
@@ -23,6 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '0g(_^t^-h=jb$*9id-1$)8uq4u0a#=d!5$rj0)qr(irh176**&'
 SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 DEBUG_PROPAGATE_EXCEPTIONS = True
